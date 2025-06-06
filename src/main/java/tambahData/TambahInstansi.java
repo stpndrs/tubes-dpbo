ed9c327b413fe49d40fd57f4c53fb9fb1e642654
fdhill <muhammadfadhilahnaf11@gmail.com>
2025-06-07 02:06:10 +0700

    update(presensi pertama menambahkan waktudimulai), tambah monitoring, perubahan nama dari update status jadi ada siswanya, update(saat admin acc kuota pada instansi akan berkurang)

---
+++ src/main/java/tambahData/TambahInstansi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tambahData;
+
+import Pengguna.Instansi;
+import com.mycompany.testdbpkl.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TambahInstansi{
+    public static void simpanInstansi(Instansi instansi) {
+        String query = "INSERT INTO instansi (nama, alamat, telpon, kouta) VALUES (?, ?, ?, ?)";
+
+        try {
+            PreparedStatement stmt = Connection.bukaKoneksi().prepareStatement(query);
+            stmt.setString(1, instansi.getNama());
+            stmt.setString(2, instansi.getAlamat());
+            stmt.setString(3, instansi.getTelpon());
+            stmt.setInt(4, instansi.getKouta());
+
+            int rows = stmt.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Data instansi berhasil ditambahkan. ");
+            } else {
+                System.out.println("Gagal menambahkan data instansi.");
+            }
+
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println("Error saat menambahkan instansi: " + e.getMessage());
+        }
+    }
+}
